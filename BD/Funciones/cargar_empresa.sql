7971ee346f4e0afaf4ad53db43da9070e133ad5b
jchacon.araya21@gmail.com <jchacon.araya21@gmail.com@ffb26ed1-f20a-8617-118d-26438c2c56cf>
2015-03-12 14:44:55 +0000

    se agregan campos de entidad empresa

--- BD/Funciones/cargar_empresa.sql
+++ BD/Funciones/cargar_empresa.sql
@@ -18,9 +18,78 @@ begin
         open empresas for
 
             select
-                num_adherente,
-                id_empresa,
-                nombre
+            num_adherente,
+			nombre,
+			id_empresa
+			direccion,
+			nom_experto,
+			razon_social,
+			cod_cartera,
+			representante,
+			contacto_representante,
+			nombre_prevencionista,
+			contacto_prevencionista,
+			masa_sum,
+			segmentacion,
+			multiregional,
+			num_centros,
+			holding,
+			num_CHPS,
+			nom_presidente,
+			nom_secretario,
+			saami,
+			amputaciones,
+			fatales,
+			proyectos_desarrollados,
+			--certificaciones CPHS
+			num_CPHScertificados ,
+			tipo_certificacion ,
+			fecha_certificacion ,
+			fecha_caducidad ,
+			--- PEC ESTANDAR
+			fecha_certificacionPECE ,
+			fecha_caducidadPECE ,
+			nivel_logroPECE ,
+			fecha_proxAuditoriaPECE ,
+			--- PEC COMPETITIVA
+			fecha_certificacionPECC ,
+			fecha_caducidadPECC ,
+			nivel_logroPECC ,
+			fecha_proxAuditoriaPECC ,
+			--- PEC EXCELENCIA
+			fecha_certificacionPECEX ,
+			fecha_caducidadPECEX ,
+			nivel_logroPECEx ,
+			fecha_proxAuditoriaPECEX ,
+			--- Accidentabilidad
+			tasa_accidentabilidad ,
+			siniestralidad ,
+			fatales_acc ,
+			invalidez ,
+			--- protocolos Prexor
+			adherenciaPREX ,
+			fecha_adherenciaPREX ,
+			fecha_vencimientoPREX ,
+			indemnizacionesPREX ,
+			num_casosIndemnizacionesPREX ,
+			pensionesPREX ,
+			num_casosPensionesPREX ,
+			actas_difusionePrex ,
+			nivel_exposicionPREX ,
+			---PLANESI
+			adherenciaPLAN ,
+			fecha_adherenciaPLAN ,
+			fecha_vencimientoPLAN ,
+			indemnizacionesPLAN ,
+			num_casosIndemnizacionPLAN ,
+			pensionesPLAN ,
+			num_casosPensionesPLAN ,
+			actas_difusionPLAN ,
+			nivel_exposicionPLAN ,
+			---PSICOSOCIAL
+			adherenciaPSICO ,
+			  ---TMERS
+			difusion_protocolo
             from 
                 empresa
             where
