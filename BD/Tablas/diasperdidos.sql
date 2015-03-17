1fde7084425d6454253071144d3810572ad63ee4
jchacon.araya21@gmail.com <jchacon.araya21@gmail.com@ffb26ed1-f20a-8617-118d-26438c2c56cf>
2015-03-17 16:10:53 +0000

    added table accidentabilidad and diasperdidos

---
+++ BD/Tablas/diasperdidos.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE public.diasperdidos (
+    id_dias 		serial not null,
+	id_empresa   	integer not null,
+    dias_perdidos	integer NOT NULL,
+	fecha_ingreso   timestamp,
+    primary key(id_dias)
+);
